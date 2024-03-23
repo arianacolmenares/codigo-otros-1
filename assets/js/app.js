6a3189b2f7311fce60934ad51ebc89f820a755ca
Ariana Colmenares <arianancr1998@gmail.com>
2024-03-22 18:22:49 -0600

    fix: correccion del codigo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,26 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const nombreElement = document.querySelector('.name'); // Cambiar el nombre de la variable , se agregó un . porque es una clase
+const blogElement = document.querySelector('.blog'); // Cambio en el nombre de la variable // Poner el . porque en el index se esta seleccionando una clase
+const locacionElement = document.querySelector('.location'); // Cambio en elS nombre de la variable
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
+async function displayUser(username) { // coloque async, para que sea una funcion asincrona porque se esta usando await  
+  nombreElement.innerHTML = 'cargando...'; // cambio en textContent por innerHTML // cambair el nombre a la variable
   const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+   console.log(data); 
+  data = await response.json(); // convertir 
+ 
+  nombreElement.innerHTML = `${data.name}`; // se cambio el apostrofe grave 
+  // cambio en textContent por innerHTML // cambio de nombre de constante
+  blogElement.innerHTML = `${data.blog}`; // se cambio el apostrofe grave  // cambio en textContent por innerHTML // cambio de nombre de constante
+  locacionElement.innerHTML = `${data.location}`; // se cambio el apostrofe grave // cambio en textContent por innerHTML //cambio de nombre de constante 
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
-}
+  nombre.innerHTML = `Algo salió mal: ${err}`
+} // cambio en textContent por innerHTML //cambio de nombre de constante 
 
-displayUser('stolinski').catch(handleError);
+
+displayUser('stolinski') .catch(handleError);
